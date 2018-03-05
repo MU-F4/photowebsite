c2e1e918e5ab1b18b11f8fd55ecbc6697677b229
MU-F4 <muhdfahmi1692@gmail.com>
2018-03-05 10:58:09 +0800

    header index and footer reload

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+  	<base href="#">
+	<meta charset="utf-8">
+	<title><?php if(isset($t)) {echo $t;} else {echo "Website Photography";} ?></title>
+	<meta name="description" content="<?php if(isset($d)) {echo $d;} else {echo "Website Photograpgy Desc";} ?>">
+	<meta property="og:image" content="#" />
+	
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Latest compiled and minified bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="/2/main.css" /> 
+
+    <!-- Icons Font -->
+    
+</head>
+
+<body>
+    
+    <section class="main-header">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                <a class="navbar-brand" href="#"><img src="/prototype/img/logo/centerlogoblack.png" class="img-responsive" alt="logo"></a>   
+                </div>
+                <div class="collapse navbar-collapse text-center" id="bs-example-navbar-collapse-1">
+                    <div class="col-md-8 col-xs-12 nav-wrap">
+                        <ul class="nav navbar-nav">
+                            <li><a href="http://prototype.stickyricetravel.com/">Home</a></li>
+                            <li><a href="http://prototype.stickyricetravel.com/about-us">About Us</a></li>
+                            <li><a href="http://prototype.stickyricetravel.com/our-adventures" >Our Adventures</a></li>
+                            <li><a href="http://prototype.stickyricetravel.com/booking" >Contact Us</a></li>
+                            <li><a href="http://prototype.stickyricetravel.com/blog">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div class="social-media hidden-sm hidden-xs hidden-md">
+                        <ul class="nav navbar-nav">
+                            <li><a href="https://www.facebook.com/StickyRiceTravel" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://twitter.com/stickyricesabah" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="https://www.instagram.com/stickyricetravel/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="https://www.youtube.com/channel/UCYbCPFEeyS2jxWqYWFaIJcg" target="_blank"><i class="fa fa fa-youtube-play"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </section>
+<div class="spacer">
+    &nbsp;
+</div>
+	
